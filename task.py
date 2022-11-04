a8618e323998ddd66b683d22cf65a9e2505d83be
pma20115 <ace20115@gmail.com>
2022-11-04 19:06:41 +0900

    言語課題

--- task.py
+++ task.py
@@ -16,7 +16,8 @@ os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 
 global interval
 
-interval = 3
+interval = 120
+relax_interval = 120
 
 scr_w, scr_h = pag.size()
 print("画面サイズの幅：", scr_w)
@@ -29,72 +30,24 @@ task_count=7
 
 def click_close():
     if messagebox.askokcancel("確認", "本当に閉じていいですか？"):
-        """cap.release()
-        out.release()
-        cv2.destroyAllWindows()
-        root.destroy()"""
         close()
         return 0
 
 def close():
-    """cap.release()
-    out.release()
-    cv2.destroyAllWindows()"""
     root.destroy()
-
-
-"""def record(cap, out, f):
-    tm=cv2.TickMeter()
-    tm.start()
-    count = 0
-    count1 = 0
-    max_count = 1
-    fps = 0
-    n = 0
-    # 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
-    while(cap.isOpened()):
-        ret, frame = cap.read()                           # フレームを取得
-        if ret == True:
-            if count == max_count:
-                tm.stop()
-                fps = max_count / tm.getTimeSec()
-                tm.reset()
-                tm.start()
-                count = 0
-            cv2.putText(frame, 'FPS: {:.2f}'.format(fps),(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), thickness=2)
-            #cv2.putText(frame, 'Frame:{:.0f}'.format(count1),(1000, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), thickness=2)
-            out.write(frame)
-            cv2.imshow('camera', frame)                            # フレームを画面に表示
-            f.write(str(count1) + "," + str(fps) + "\n")
-            count1 += 1
-            count += 1
-            # キー操作があればwhileループを抜ける
-            k = cv2.waitKey(33)
-            if k == 27:    # Esc key to stop
-                break
-        else:
-            break
-    # 撮影用オブジェクトとウィンドウの解放
-    f.close()
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()"""
-
 
 def QUESTION():
 
-    one = random.randint(11, 30)
-    two = random.randint(1, 10)
+    one = random.randint(10, 90)
+    two = random.randint(10, 90)
 
-    hugo_index = random.randint(0, 2)
-    random_radiobutton = random.randint(0, 3)
-    hugo = ["×", "-", "+"]
-    ans = [one * two, one - two, one + two]
+    hugo = ["×"]
+    ans = [one * two]
 
     # 問題をランダムに生成
-    hugo = hugo[hugo_index]
-    ans = ans[hugo_index]
-    radio_button_list = [ans, ans * 10, ans - 10, ans * 5]
+    hugo = hugo[0]
+    ans = ans[0]
+    radio_button_list = [ans, ans - 10, ans + 10, ans + two]
     random.shuffle(radio_button_list)  # 配列の中をシャッフル
     question = "{} {} {} = ".format(one, hugo, two)
 
@@ -103,6 +56,13 @@ def QUESTION():
 
     return ans, question, radio_button_list
 
+def question_language():
+
+    ans = "しゃべる"
+    question = "話す"
+    radio_button_list = ["笑う", "歌う", "なく", "しゃべる"]
+
+    return ans, question, radio_button_list
 
 ####切り替えボタン####
 def change():
@@ -153,7 +113,7 @@ def timecount(canvas, video, audio):
         # time_label.configure(text=f"経過時間：{str(elapsed_minute).zfill(2)}:{str(elapsed_second).zfill(2)}")
 
         # print(interval)
-        if second == interval:
+        if second == relax_interval:
             # logに書き込み
             log.logging(
                 situation="リラックスモード終了",
@@ -310,8 +270,8 @@ def task_select():
     elif count % 2 == 0:
         eye_task(master=canvas1)
     else:
-        Application(master=canvas1)
-        #eye_task(master=canvas1)
+        #Application(master=canvas1)
+        Language(master=canvas1)
     # print(App)
 
 ####アンケート評価####
@@ -584,7 +544,7 @@ class eye_task(tk.Frame):
         self.flg = True
         # print(self.second)
         while self.flg:
-            #print(self.second)
+            # print(self.second)
             self.second += 1
             time.sleep(1)
 
@@ -619,7 +579,7 @@ class eye_task(tk.Frame):
 
     def change_label_text(self):
         self.text = self.random_symbol()
-        #print(self.flg)
+        print(self.flg)
         if self.flg == True:
             self.master.delete("line")
             for i in range(4):
@@ -633,10 +593,8 @@ class eye_task(tk.Frame):
                     sticky="nsew",
                 )
 
-
+"""
 ####計算課題####
-
-
 class Application(tk.Frame):
     def __init__(self, master):
         # logに書き込み
@@ -948,6 +906,339 @@ class Application(tk.Frame):
                     choice_4="-",
                 )
 
+                self.q_label2.configure(text="")
+                messagebox.showinfo(
+                    "リザルト",
+                    f"あなたのスコアは{self.correct_cnt}/{self.index}問正解です。\nクリアタイムは{self.second}秒です。",
+                )
+
+                # logに書き込み
+                # root.destroy()
+
+                # quit_me(root)
+                # sys.exit(0)
+                self.second = 0
+                self.destroy()
+                self.master.destroy()
+
+                global count
+                count += 1
+                questionnaire()
+                return 0
+
+"""
+####言語課題####
+class Language(tk.Frame):
+    def __init__(self, master):
+        # logに書き込み
+        log.logging(
+            situation="言語課題の説明",
+            action="-",
+            user_input="-",
+            correct="-",
+            judge="-",
+            evaluation="-",
+            eye_ans="-",
+            ans_position="-",
+            eye_correct="-",
+            choice_1="-",
+            choice_2="-",
+            choice_3="-",
+            choice_4="-",
+        )
+
+        super().__init__(master)
+        self.pack()
+
+        # 問題数インデックス
+        self.index = 0
+
+        # 正解数カウント用
+        self.correct_cnt = 0
+
+
+        # self.pack()
+        self.column_data = (0, 0, 1, 1)
+        self.row_data = (0, 1, 0, 1)
+        # 各種ウィジェットの作成
+        self.label1_frame_app = tk.Label(self.master, text="言語課題", font=("", 40))#,fg="red")
+        self.label2_frame_app = tk.Label(self.master, text="4択のうち正しい答えを選択し決定してください", font=("", 40))
+        self.label3_frame_app = tk.Label(self.master, text="1問あたり制限時間5秒で答えてください。全部で24問です。", font=("", 30))
+        self.button_change_frame_app = tk.Button(
+            self.master, text="課題に進む", font=("", 40), bg="grey", command=lambda: self.rocate(),relief="solid"
+        )
+        # 各種ウィジェットの設置
+        self.label1_frame_app.pack(anchor="center", expand=1)
+        self.label2_frame_app.pack(anchor="center", expand=1)
+        self.label3_frame_app.pack(anchor="center", expand=1)
+        self.button_change_frame_app.pack(anchor="center", expand=1)
+
+
+    def rocate(self):
+        # logに書き込み
+        log.logging(
+            situation="言語課題の説明",
+            action="進むボタンを押した",
+            user_input="-",
+            correct="-",
+            judge="-",
+            evaluation="-",
+            eye_ans="-",
+            ans_position="-",
+            eye_correct="-",
+            choice_1="-",
+            choice_2="-",
+            choice_3="-",
+            choice_4="-",
+        )
+        self.label1_frame_app.pack_forget()
+        self.label2_frame_app.pack_forget()
+        self.label3_frame_app.pack_forget()
+        self.button_change_frame_app.pack_forget()
+        self.create_widgets()
+
+        # 経過時間スレッドの開始
+        self.t = threading.Thread(target=self.timer, daemon=True)
+        self.t.start()
+
+        # Tkインスタンスに対してキーイベント処理を実装
+        # self.master.bind("<KeyPress>", self.type_event)
+
+    # ウィジェットの生成と配置
+    def create_widgets(self):
+        # logに書き込み
+        log.logging(
+            situation="言語課題の開始",
+            action="-",
+            user_input="-",
+            correct="-",
+            judge="-",
+            evaluation="-",
+            eye_ans="-",
+            ans_position="-",
+            eye_correct="-",
+            choice_1="-",
+            choice_2="-",
+            choice_3="-",
+            choice_4="-",
+        )
+
+        # # 時間計測用のラベル
+        self.time_label = tk.Label(self, text="", font=("", 20))
+        self.time_label.grid(row=0, column=0, columnspan=2)
+
+        self.ans_label2 = tk.Label(self, text="", width=10, anchor="w", font=("", 40))
+        self.ans_label2.grid(row=1, column=0)
+        self.q_label = tk.Label(self, text="Q.同義語を選んでください", font=("", 30))
+        self.q_label.grid(row=2, column=0)
+
+        # 問題作成
+        self.ans, self.q, self.radio_button_list = question_language()
+        self.ans_label3 = tk.Label(self, text="", width=10, anchor="w", font=("", 15))
+        self.ans_label3.grid(row=3, column=0)
+        self.q_label2 = tk.Label(self, text=self.q, width=10, font=("", 30))
+        self.q_label2.grid(row=4, column=0)
+
+
+        # ウィジェットの作成
+        self.radio_value = tk.IntVar()  # ラジオボタンの初期値を0にする
+
+        # ラジオボタンの作成
+        self.radio0 = tk.Radiobutton(
+            self.master,
+            text=str(self.radio_button_list[0]),  # ラジオボタンの表示名
+            command=self.radio_click,  # クリックされたときに呼ばれるメソッド
+            variable=self.radio_value,  # 選択の状態を設定する
+            value=0,  # ラジオボタンに割り付ける値の設定
+            indicator=0,
+            background="light blue",
+            font=("", 20),
+            width=20,
+            height=3,
+        )
+
+        self.radio1 = tk.Radiobutton(
+            self.master,
+            text=str(self.radio_button_list[1]),  # ラジオボタンの表示名
+            command=self.radio_click,  # クリックされたときに呼ばれるメソッド
+            variable=self.radio_value,  # 選択の状態を設定する
+            value=1,  # ラジオボタンに割り付ける値の設定
+            indicator=0,
+            background="light blue",
+            font=("", 20),
+            width=20,
+            height=3,
+        )
+
+        self.radio2 = tk.Radiobutton(
+            self.master,
+            text=str(self.radio_button_list[2]),  # ラジオボタンの表示名
+            command=self.radio_click,  # クリックされたときに呼ばれるメソッド
+            variable=self.radio_value,  # 選択の状態を設定する
+            value=2,  # ラジオボタンに割り付ける値の設定
+            indicator=0,
+            background="light blue",
+            font=("", 20),
+            width=20,
+            height=3,
+        )
+
+        self.radio3 = tk.Radiobutton(
+            self.master,
+            text=str(self.radio_button_list[3]),  # ラジオボタンの表示名
+            command=self.radio_click,  # クリックされたときに呼ばれるメソッド
+            variable=self.radio_value,  # 選択の状態を設定する
+            value=3,  # ラジオボタンに割り付ける値の設定
+            indicator=0,
+            background="light blue",
+            font=("", 20),
+            width=20,
+            height=3,
+        )
+
+        # ボタンの作成
+        self.button = tk.Button(
+            self.master,
+            text="OK",  # ボタンの表示名
+            command=self.button_click,  # クリックされたときに呼ばれるメソッド
+            relief="solid",
+            state=tk.DISABLED,
+            width=20,
+            height=3,
+            bg="grey",
+        )
+
+        # ボタンクリックに対してキーイベント処理を実装
+        # self.button.bind("<ButtonPress>", self.type_event)
+
+        # ウィジェットの設置
+        self.radio0.pack()
+        self.radio1.pack()
+        self.radio2.pack()
+        self.radio3.pack()
+        self.button.pack(expand=1)
+
+        self.result_label = tk.Label(self, text="", font=("", 20))
+        self.result_label.grid(row=5, column=0, columnspan=2)
+
+        self.flg2 = True
+
+    def radio_click(self):
+        # ラジオボタンの値を取得
+        value = self.radio_value.get()
+        print(f"ラジオボタンの値は {self.radio_button_list[value]} です")
+        #self.answer.configure(text=value)
+        if self.button["state"] == tk.DISABLED:
+            self.button["state"] = tk.NORMAL
+            self.button["bg"] = "white"
+
+        # "OK"のボタンを押したかどうか
+        self.next = False
+
+    # def button_click(self):
+    #     # 問題を次に移動させるフラグを立てる
+    #     value = self.radio_value.get()
+    #     self.ans_label2 = value
+    #     self.next = True
+
+    # キー入力時のイベント処理
+    def button_click(self):
+        global log
+        value = self.radio_value.get()
+        self.ans_label2 = value
+
+        if self.button["state"] == tk.NORMAL:
+            self.button["state"] = tk.DISABLED
+            self.button["bg"] = "grey"
+
+        # 入力値の答え合わせ
+        print("OKボタンを押しました")
+        print(f"押した答え:{self.ans_label2},本当の答え：{self.ans}")
+        if str(self.ans) == str(self.radio_button_list[self.ans_label2]):
+            # logに書き込み
+            log.logging(
+                situation="集中",
+                action="userがボタン入力",
+                user_input=str(self.radio_button_list[self.ans_label2]),
+                correct=str(self.ans),
+                judge="正解",
+                evaluation="-",
+                eye_ans="-",
+                ans_position="-",
+                eye_correct="-",
+                choice_1="-",
+                choice_2="-",
+                choice_3="-",
+                choice_4="-",
+            )
+
+            self.result_label.configure(text="正解！", fg="red")
+            self.correct_cnt += 1
+        else:
+            # logに書き込み
+            log.logging(
+                situation="集中",
+                action="userがボタン入力",
+                user_input=str(self.radio_button_list[self.ans_label2]),
+                correct=str(self.ans),
+                judge="不正解",
+                evaluation="-",
+                eye_ans="-",
+                ans_position="-",
+                eye_correct="-",
+                choice_1="-",
+                choice_2="-",
+                choice_3="-",
+                choice_4="-",
+            )
+
+            self.result_label.configure(text="残念！", fg="blue")
+
+        # 次の問題を出題
+        self.index += 1
+        self.ans, q, radio_button_list = QUESTION()
+        self.q_label2.configure(text=q)
+        self.radio0.configure(
+            text=str(radio_button_list[0]), value=str(radio_button_list[0])
+        )
+        self.radio1.configure(
+            text=str(radio_button_list[1]), value=str(radio_button_list[1])
+        )
+        self.radio2.configure(
+            text=str(radio_button_list[2]), value=str(radio_button_list[2])
+        )
+        self.radio3.configure(
+            text=str(radio_button_list[3]), value=str(radio_button_list[3])
+        )
+        # "OK"のボタンを押したかどうか
+        self.next = False
+
+    def timer(self):
+        self.second = 0
+        self.flg = True
+        while self.flg:
+            self.second += 1
+            self.time_label.configure(text=f"経過時間：{self.second}秒")
+            time.sleep(1)
+
+            # 2分経ったら
+            if self.second == interval:
+                # logに書き込み
+                log.logging(
+                    situation="言語課題の終了",
+                    action="-",
+                    user_input="-",
+                    correct="-",
+                    judge="-",
+                    evaluation="-",
+                    eye_ans="-",
+                    ans_position="-",
+                    eye_correct="-",
+                    choice_1="-",
+                    choice_2="-",
+                    choice_3="-",
+                    choice_4="-",
+                )
 
                 self.q_label2.configure(text="")
                 messagebox.showinfo(
@@ -968,6 +1259,7 @@ class Application(tk.Frame):
                 count += 1
                 questionnaire()
                 return 0
+
 
 # log
 class Log:
@@ -993,10 +1285,10 @@ class Log:
         self.choice_4 = choice_4
 
         filepath = f".\\log_dir\\{self.first_time}.csv"
-        columns = ["時間", "状態", "アクション", "ユーザーの入力", "正解値", "正誤判定","集中度自己評価","ユーザーの答え","答えの位置","正解","第1象限","第2象限","第3象限","第4象限"]
+        columns = ["時間", "状態", "アクション", "計算課題の入力", "正解値", "正誤判定","集中度自己評価","視線課題の入力","答えの位置","正解","第1象限","第2象限","第3象限","第4象限"]
 
         dt_now = datetime.datetime.now()
-        time = dt_now.strftime('%Y_%m_%d_%H.%M.%S.%f')[:-3]
+        time = dt_now.strftime('%Y_%m_%d_%H.%M.%S')
         print(time)
         # self.log_data = {
         #     "time": [],
@@ -1010,11 +1302,11 @@ class Log:
             "時間": [],
             "状態": [],
             "アクション": [],
-            "ユーザーの入力": [],
+            "計算課題の入力": [],
             "正解値": [],
             "正誤判定": [],
             "集中度自己評価": [],
-            "ユーザーの答え": [],
+            "視線課題の入力": [],
             "答えの位置": [],
             "正解": [],
             "第1象限": [],
@@ -1032,11 +1324,11 @@ class Log:
         self.log_data["時間"].append(time)
         self.log_data["状態"].append(self.situation)
         self.log_data["アクション"].append(self.action)
-        self.log_data["ユーザーの入力"].append(self.user_input)
+        self.log_data["計算課題の入力"].append(self.user_input)
         self.log_data["正解値"].append(self.correct)
         self.log_data["正誤判定"].append(self.judge)
         self.log_data["集中度自己評価"].append(self.evaluation)
-        self.log_data["ユーザーの答え"].append(self.eye_ans)
+        self.log_data["視線課題の入力"].append(self.eye_ans)
         self.log_data["答えの位置"].append(self.ans_position)
         self.log_data["正解"].append(self.eye_correct)
         self.log_data["第1象限"].append(self.choice_1)
@@ -1064,43 +1356,6 @@ class Log:
 
 
 if __name__ == "__main__":
-    """#dt_now = datetime.datetime.now()
-    dt_before = datetime.datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    #dt_before = datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    print("カメラを起動した時刻"+str(dt_before))
-    cap = cv2.VideoCapture(1)
-
-    fps = 30
-    w = 1280
-    h = 720
-    #cap.set(cv2.CAP_PROP_FPS, fps)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'));
-    # 動画ファイル保存用の設定
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    print(fps)# カメラのFPSを取得
-    int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))              # カメラの横幅を取得
-    int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))             # カメラの縦幅を取得
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')     # 動画保存時のfourcc設定（mp4用）
-
-    dt_after = datetime.datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    #dt_after = datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    print("カメラを起動後の時刻"+str(dt_after))
-    name="./camera/test"
-    txt_name = str(name)+'.txt'
-    f = open(str(txt_name), 'w')
-    print(f)
-    camera_name = str(name)+'_video.mp4'
-    out = cv2.VideoWriter(str(camera_name), fourcc, fps, (w, h))  # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
-    dt_after = datetime.datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    #dt_after = datetime.now().strftime('%Y_%b_%d_%H.%M.%S.%f')[:-3]
-    print("カメラを起動後の時刻"+str(dt_after))
-    f.write("カメラを起動した時刻,"+str(dt_before)+"\nカメラを起動後の時刻,"+str(dt_after)+"\n解像度,"+str(w)+"×"+str(h)+"\n動画FPS,"+str(fps)+"\n")
-    f.write("フレーム数,FPS\n")
-    thread = threading.Thread(name="thread", target=record, args=[cap, out, f], daemon=True)
-    thread.start()"""
-
     root = tk.Tk()
     # root.geometry("1280x720")
     # root.state("zoomed")
@@ -1110,7 +1365,7 @@ if __name__ == "__main__":
     root.title("タイピングゲーム！")
 
     dt_now = datetime.datetime.now()
-    now = dt_now.strftime('%Y_%m_%d_%H.%M.%S.%f')[:-3]
+    now = dt_now.strftime('%Y_%m_%d_%H.%M.%S')[:-3]
     global log
     log = Log()
     log.first_log(now)
